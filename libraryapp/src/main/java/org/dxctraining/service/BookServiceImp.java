20d002d4683fb271a6d905568127f9b2ca3eeb13
lucifer <lucifer@DESKTOP-11SDPPU>
2020-08-10 08:40:23 +0530

    aa

---
+++ libraryapp/src/main/java/org/dxctraining/service/BookServiceImp.java
@@ -0,0 +1,81 @@
+package org.dxctraining.service;
+import org.dxctraining.dao.*;
+import org.dxctraining.entities.*;
+import org.dxctraining.exceptions.*;
+
+import java.util.List;
+
+
+
+
+
+public class BookServiceImp implements IBookService {
+	private IBookDao dao = new BookDaoImp();
+
+	@Override
+	public Book findById(String id) {
+		validateId(id);
+		Book book = dao.findBookById(id);
+		return book;
+	}
+
+	@Override
+	public void addBook(Book book) {
+		dao.addBook(book);
+	}
+
+	@Override
+	public void remove(String id) {
+		validateId(id);
+		dao.remove(id);
+	}
+
+	@Override
+	public Book updateCost(String id, double cost) {
+		validateId(id);
+		validateCost(cost);
+		Book book = dao.updateCost(id, cost);
+		return book;
+	}
+/*
+	@Override
+	public Book updateBookName(String id, String name) {
+		validateId(id);
+		validateName(name);
+		Book book = dao.updateBookName(id, name);
+		return book;
+	}
+	@Override
+	public void bookDetails(Book book) {
+		dao.bookDetails(book);
+	}
+*/
+	@Override
+	public List<Book> findAllBook() {
+		List<Book> book = dao.findAllBooks();
+		return book;
+
+	}
+
+	void validateName(String name) {
+		if (name == null || name.isEmpty()) {
+			throw new InvalidArgumentException("Book name can't be null or empty");
+		}
+	}
+
+	void validateId(String id) {
+		if (id == null || id.isEmpty()) {
+			throw new InvalidArgumentException("Book Id can't be null or empty");
+		}
+	}
+
+	void validateCost(double cost) {
+		if (cost < 0) {
+			throw new InvalidCostException("cost can not be -ve");
+		}
+
+	}
+
+
+
+}
